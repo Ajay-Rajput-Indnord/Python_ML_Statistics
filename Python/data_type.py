a55cbff759eae4226e6f2db886af92ace18b3aba
Ajay-Rajput-Indnord <Ajay.Rajput@indnord.com>
2026-08-05 20:13:23 +0530

    first commit

---
+++ Python/data_type.py
@@ -0,0 +1,12 @@
+'''Inside the editor, complete the following steps:
+1.Create a variable x with the value 5
+2.Create a variable y with the value 3.14
+3.Create a variable z with the value "Hello"
+4.Print the data type of each variable using type()'''
+
+x=5
+y=3.14
+z='Hello'
+print(type(x))
+print(type(y))
+print(type(z))
